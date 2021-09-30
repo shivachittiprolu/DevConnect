35ee27287e0706badb7cd1f2d90193f2c13e911a
shiva chittiprolu <shivachittiprolu@gmail.com>
2021-09-30 23:18:24 +0530

    added routes for user,auth,posts and profile

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 
+
 const app = express();
 
 //connect to Database
@@ -9,8 +10,13 @@ connectDB();
 
 app.get('/',(req, res)=>{
     res.send("Hello World!");
-})
+});
 
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 
 const PORT = process.env.PORT || 5000; //PORT 
