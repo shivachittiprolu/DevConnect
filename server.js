b69aa81b284c7bfa2dfb290f76d0573a1eea43ee
shiva chittiprolu <shivachittiprolu@gmail.com>
2021-09-30 22:21:18 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req, res)=>{
+    res.send("Hello World!");
+})
+
+
+
+const PORT = process.env.PORT || 5000; //PORT 
+
+app.listen((PORT), () => console.log(`Listening on ${PORT}`));
