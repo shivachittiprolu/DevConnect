a6a25842fd386b0c8ec920f400953e27aef1ad16
shiva chittiprolu <shivachittiprolu@gmail.com>
2021-10-06 09:40:37 +0530

    Authentication - using Middleware

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+const User = require('../../models/User');
 
 // @route api/auth
 // @desc Test route
 // @access Public
-router.get('/', (req, res) => {
-    res.send('auth Route');
+router.get('/', auth, async (req, res) => {
+    res.send('Token Worked');
+    console.log(req.user);
+    // try {
+    //     console.log(`user id: ${req}`);
+    //     const user = await User.findById(req.user.id).select('-password');
+    //     res.json(user);
+    // } catch (err) {
+    //     console.error(err.messsage);
+    //     res.status(500).send('Server Error');   
+    // }
 });
 
 module.exports = router;
